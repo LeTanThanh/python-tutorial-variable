e9ed06e50d1b818c5a8fb9567ae583bbfd0ecdf4
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-26 20:57:09 +0700

    Naming variables

--- main.py
+++ main.py
@@ -15,3 +15,22 @@ if __name__ == "__main__":
 
   counter = 1
   print(counter)
+
+  # Naming variables
+
+  """
+  - Variable names can contain only letters, numbers, and underscores (_).
+  They can start with a letter or an underscore (_), not with a number.
+
+  - Variable names cannot contains space.
+  To separate words in variable, you use underscore for example sorted_list
+
+  - Variable names cannot be the same as keywords, reserved words, and built-in functions in Python.
+
+  - Variable names should be concise and descriptive.
+  For example, the active_user variable is more descriptive than the au.
+
+  - Use underscore (_) to separate multiple words in the variable names.
+
+  - Avoid using the letter l and the upper case letter O because they look like the number 1 and 0.
+  """
